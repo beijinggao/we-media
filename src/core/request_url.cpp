9e2aa962bc56ca9e21e1a73e5780e5bb921bdf36
gao8954 <895407214gao@sina.com>
2018-08-29 16:17:16 +0800

    find the headers

--- src/core/request_url.cpp
+++ src/core/request_url.cpp
@@ -8,26 +8,25 @@
 
 void RequestUrl::parseUrl(std::string url) {
     // Create a CefRequest object.
-    CefRefPtr <CefRequest> request = CefRequest::Create();
+    CefRefPtr<CefRequest> request = CefRequest::Create();
 
-// Set the request URL.
+    // Set the request URL.
     request->SetURL(url);
 
-// Set the request method. Supported methods include GET, POST, HEAD, DELETE and PUT.
-    request->SetMethod(“POST”);
+    // Set the request method. Supported methods include GET, POST, HEAD, DELETE and PUT.
+    request->SetMethod("POST");
 
-// Optionally specify custom headers.
+    // Optionally specify custom headers.
     CefRequest::HeaderMap headerMap;
-    headerMap.insert(
-            std::make_pair("X-My-Header", "My Header Value"));
+    headerMap.insert(std::make_pair("X-My-Header","My Header Value"));
     request->SetHeaderMap(headerMap);
 
-// Optionally specify upload content.
-// The default “Content-Type” header value is "application/x-www-form-urlencoded".
-// Set “Content-Type” via the HeaderMap if a different value is desired.
-    const std::string &upload_data = “arg1 = val1 & arg2 = val2”;
-    CefRefPtr <CefPostData> postData = CefPostData::Create();
-    CefRefPtr <CefPostDataElement> element = CefPostDataElement::Create();
+    // Optionally specify upload content.
+    // The default “Content-Type” header value is "application/x-www-form-urlencoded".
+    // Set “Content-Type” via the HeaderMap if a different value is desired.
+    const std::string &upload_data = "arg1 = val1 & arg2 = val2";
+    CefRefPtr<CefPostData> postData = CefPostData::Create();
+    CefRefPtr<CefPostDataElement> element = CefPostDataElement::Create();
     element->SetToBytes(upload_data.size(), upload_data.c_str());
     postData->AddElement(element);
     request->SetPostData(postData);
